fbb398f969510e254830c2b0925e7883817a4a3c
k4rldoherty <karldoherty15@icloud.com>
2026-02-20 20:36:35 +0000

    Added try in event price alert does not send

--- src/Stocki.Discord/Handlers/PriceAlertHandler.cs
+++ src/Stocki.Discord/Handlers/PriceAlertHandler.cs
@@ -27,26 +27,34 @@ public class PriceAlertHandler : INotificationHandler<PriceAlertNotification>
             var users = await repo.GetAllUsersSubscribedToAStock(notification.Symbol, token);
             foreach (var u in users)
             {
-                var userInfo = await _discordClient.GetUserAsync(u);
-                if (userInfo == null)
+                try
                 {
-                    _logger.LogWarning("User info is null for user id {}", u);
-                    continue;
+
+                    var userInfo = await _discordClient.GetUserAsync(u);
+                    if (userInfo == null)
+                    {
+                        _logger.LogWarning("User info is null for user id {}", u);
+                        continue;
+                    }
+                    var dmChannel = await userInfo.CreateDMChannelAsync();
+                    await dmChannel.SendMessageAsync(
+                        embed: new EmbedBuilder()
+                            .WithTitle($"Price Notification for {notification.Symbol}")
+                            .AddField("New Price", $"${notification.Price}")
+                            .AddField(
+                                "Percent Change",
+                                $"{String.Format("{0:0.00}", notification.PercentChange)}%"
+                            )
+                            .WithFooter("Stocki 2025")
+                            .WithColor(Color.Green)
+                            .Build()
+                    );
+                    _logger.LogInformation("Message sent to user {} successfully", u);
                 }
-                var dmChannel = await userInfo.CreateDMChannelAsync();
-                await dmChannel.SendMessageAsync(
-                    embed: new EmbedBuilder()
-                        .WithTitle($"Price Notification for {notification.Symbol}")
-                        .AddField("New Price", $"${notification.Price}")
-                        .AddField(
-                            "Percent Change",
-                            $"{String.Format("{0:0.00}", notification.PercentChange)}%"
-                        )
-                        .WithFooter("Stocki 2025")
-                        .WithColor(Color.Green)
-                        .Build()
-                );
-                _logger.LogInformation("Message sent to user {} successfully", u);
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error sending message to user {User}", u);
+                }
             }
         }
     }
