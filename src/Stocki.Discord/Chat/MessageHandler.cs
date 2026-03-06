26e816ba424ce4388b2de5addc15991391727f8a
k4rldoherty <karldoherty15@icloud.com>
2026-03-06 11:08:55 +0000

    Added some custom instructions and thinking config to model to improve responses

--- src/Stocki.Discord/Chat/MessageHandler.cs
+++ src/Stocki.Discord/Chat/MessageHandler.cs
@@ -6,9 +6,11 @@ namespace Stocki.Discord.Chat;
 public class MessageHandler
 {
     private readonly IGeminiClient _client;
-    public MessageHandler(IGeminiClient client)
+    private readonly ILogger<MessageHandler> _logger;
+    public MessageHandler(IGeminiClient client, ILogger<MessageHandler> logger)
     {
         _client = client;
+        _logger = logger;
     }
     public Task HandleMessageAsync(SocketMessage msg)
     {
@@ -18,9 +20,23 @@ public class MessageHandler
 
         _ = Task.Run(async () =>
             {
-                var cts = new CancellationTokenSource(TimeSpan.FromSeconds(15));
-                var response = await _client.GetResponseAsync(msg.Content, cts.Token);
-                await msg.Channel.SendMessageAsync(response);
+                try
+                {
+
+                    using (var channel = msg.Channel.EnterTypingState())
+                    {
+
+                        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(45));
+                        var response = await _client.GetResponseAsync(msg.Content, cts.Token);
+                        if (response.Length > 2000)
+                            await msg.Channel.SendMessageAsync(response[..1997] + "...");
+                        else await msg.Channel.SendMessageAsync(response);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error handling message");
+                }
             });
         return Task.CompletedTask;
     }
--- src/Stocki.Infrastructure/Clients/Gemini/GeminiClient.cs
+++ src/Stocki.Infrastructure/Clients/Gemini/GeminiClient.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Stocki.Shared.Config;
 using Microsoft.Extensions.Options;
 using Stocki.Application.Interfaces;
+using Google.GenAI.Types;
 namespace Stocki.Infrastructure.Clients;
 
 public class GeminiClient : IGeminiClient
@@ -19,7 +20,40 @@ public class GeminiClient : IGeminiClient
 
     public async ValueTask<string> GetResponseAsync(string prompt, CancellationToken c)
     {
-        var response = await _client.Models.GenerateContentAsync(model: _settings.Value.Model, contents: prompt, config: null, c);
-        return response.Candidates?[0].Content?.Parts?[0].Text ?? "Could not generate a response.";
+        var config = new GenerateContentConfig
+        {
+            SystemInstruction = new Content
+            {
+                Parts =
+                [
+                  new Part
+                  {
+                    Text = $@"
+                    You are a sarcastically witty stock market assistant. 
+                    Today's date is {DateTime.Now}.
+                    1. ONLY answer stock/finance questions. For anything else, say: 'I only talk stocks', but in a sarcastic or witty way.
+                    2. If you don't know an answer or lack real-time data for a specific price, say: 'I don't have that information.'
+                    3. Do not speculate on out-of-date events.
+                    4. Keep your answers short and to the point, very concise and simple"
+                  }
+                ]
+            },
+            MaxOutputTokens = 2000,
+            ThinkingConfig = new ThinkingConfig
+            {
+                ThinkingLevel = ThinkingLevel.Medium,
+            }
+        };
+        try
+        {
+
+            var response = await _client.Models.GenerateContentAsync(model: _settings.Value.Model, contents: prompt, config: config, cancellationToken: c);
+            return response.Candidates?[0].Content?.Parts?[0].Text ?? "Could not generate a response.";
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error generating response");
+            return "I'm done speaking for the day, go flip some flapjacks because my owner is too stingy to pay for more tokens";
+        }
     }
 }
